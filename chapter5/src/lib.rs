e92fd27c3c75f243e8e0765880aa45fa80084d77
brook-bang <1455047806@qq.com>
2025-02-14 15:29:38 +0800

    finish chapter 5

--- chapter5/src/lib.rs
+++ chapter5/src/lib.rs
@@ -80,7 +80,15 @@ pub fn generate_html_template_var(
     content: &mut ExpressionData,
     context: HashMap<String,String>,
 ) -> &mut ExpressionData {
-    
+    content.gen_html = content.expression.clone();
+    for var in &content.var_map {
+        let (_h,i) = get_index_for_symbol(&var, '{');
+        let (_j,k) = get_index_for_symbol(&var, '}');
+        let var_without_braces = &var[i + 2..k];
+        let val = context.get(var_without_braces).unwrap();
+        content.gen_html = content.gen_html.replace(var, val);
+    }
+    content
 }
 
 pub fn get_expression_data(input_line: &str) -> ExpressionData {
@@ -97,6 +105,68 @@ pub fn get_expression_data(input_line: &str) -> ExpressionData {
         expression: input_line.into(),
         var_map: template_var_map,
         gen_html: "".into(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_get_index_for_symbol_test() {
+        assert_eq!((true,3),get_index_for_symbol("Hi {name} bye", '{'));
+    }
+
+    #[test]
+    fn check_template_var_test() {
+        let content = "Hi {{name}} bye".into();
+        let expr_data = ExpressionData {
+            expression: content,
+            var_map: vec!["{{name}}".to_string()],
+            gen_html: "".into(),
+        };
+        assert_eq!(
+            ContentType::TemplateVariable(expr_data),
+            get_content_type("Hi {{name}} bye")
+        );
+    }
+
+    #[test]
+    fn check_if_tag_test(){
+        assert_eq!(
+            ContentType::Tag(TagType::IfTag),
+            get_content_type("{% if name == 'Bob' %}")
+        );
+    }
+
+    #[test]
+    fn check_literal_test() {
+        let s = "<h1>Hello world</h1>";
+        assert_eq!(ContentType::Literal(s.to_string()),get_content_type(s));
+    }
+
+    #[test]
+    fn check_get_expression_data_test() {
+        let map_var = vec!["{{name}}".to_string(),"{{city}}".to_string()];
+        let expression_data = ExpressionData {
+            expression: "Hi {{name}} , welcome to {{city}}".into(),
+            var_map: map_var,
+            gen_html: "".into(),
+        };
+        assert_eq!(
+            expression_data,
+            get_expression_data("Hi {{name}} , welcome to {{city}}")
+        );
+    }
+
+    #[test]
+    fn check_symbol_string_test(){
+        assert_eq!(true,check_symbol_string("{{Hello}}", "{{"));
+    }
+
+    #[test]
+    fn check_symbol_pair_test() {
+        assert_eq!(true,check_matching_pair("{{hello}}", "{{", "}}"));
     }
 }
 
--- chapter5/src/main.rs
+++ chapter5/src/main.rs
@@ -1,13 +1,33 @@
-use std::{collections::HashMap, io::{self, BufRead}};
+use std::{
+    collections::HashMap,
+    hint,
+    io::{self, BufRead},
+};
+
+use chapter5::{generate_html_template_var, get_content_type, ContentType, TagType};
 
 fn main() {
-    let mut context: HashMap<String,String> = HashMap::new();
+    let mut context: HashMap<String, String> = HashMap::new();
     context.insert("name".to_string(), "Bob".to_string());
     context.insert("city".to_string(), "Boston".to_string());
 
-    for line in io::stdin().lock().lines(){
-        
+    for line in io::stdin().lock().lines() {
+        match get_content_type(&line.unwrap().clone()) {
+            ContentType::TemplateVariable(mut content) => {
+                let html = generate_html_template_var(&mut content, context.clone());
+                println!("{}", html.gen_html);
+            }
+            ContentType::Literal(text) => println!("{}", text),
+            ContentType::Tag(TagType::ForTag) => println!("For Tag not implemented"),
+            ContentType::Tag(TagType::IfTag) => println!("If Tag not implemented"),
+            ContentType::Unrecognized => println!("Unrecognized input"),
+        }
     }
-
-
 }
+
+/*
+Hi {{name}}, welcome to {{city}}!
+<h1>Hello, world!</h1>
+{% for name in names %}
+{% if name == 'Bob' %}
+*/
